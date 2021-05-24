1d09609e3cb62adbabb3858ae55bc9512a22a1d0
jakhremchik <jakhremchik@lanit-tercom.com>
2021-05-24 12:04:26 +0300

    scripts modification

--- scripts/init_drone_ws.sh
+++ scripts/init_drone_ws.sh
@@ -14,6 +14,14 @@ tmux send-keys "source $source_path" C-m
 tmux send-keys "sleep 0" C-m
 tmux send-keys "roslaunch mavros apm.launch" C-m
 
+tmux selectp -t 0
+# tmux split -h -p 33
+tmux send-keys "export ROS_IP=$ROS_IP" C-m
+tmux send-keys "export ROS_MASTER_URI=$ROS_MASTER_URI" C-m
+tmux send-keys "source $source_path" C-m
+tmux send-keys "sleep 10" C-m
+tmux send-keys "rosservice call /mavros/set_stream_rate 0 10 1" C-m
+
 
 tmux new-window  -t Drone:1
 tmux rename-window 'Drone:|RS_265|RS_265_UNDISTORT|ORB_SLAM|'
@@ -24,7 +32,7 @@ tmux split -v -p 50
 tmux send-keys "export ROS_IP=$ROS_IP" C-m
 tmux send-keys "export ROS_MASTER_URI=$ROS_MASTER_URI" C-m
 tmux send-keys "source $source_path" C-m
-tmux send-keys "sleep 2" C-m
+tmux send-keys "sleep 3" C-m
 tmux send-keys "roslaunch realsense2_camera rs_t265.launch" C-m
 
 tmux selectp -t 0
@@ -32,7 +40,7 @@ tmux split -h -p 33
 tmux send-keys "export ROS_IP=$ROS_IP" C-m
 tmux send-keys "export ROS_MASTER_URI=$ROS_MASTER_URI" C-m
 tmux send-keys "source $source_path" C-m
-tmux send-keys "sleep 4" C-m
+tmux send-keys "sleep 5" C-m
 tmux send-keys "roslaunch vision_to_mavros t265_tf_to_mavros.launch" C-m
 
 tmux selectp -t 0
@@ -40,7 +48,7 @@ tmux split -h -p 33
 tmux send-keys "export ROS_IP=$ROS_IP" C-m
 tmux send-keys "export ROS_MASTER_URI=$ROS_MASTER_URI" C-m
 tmux send-keys "source $source_path" C-m
-tmux send-keys "sleep 3" C-m
+tmux send-keys "sleep 6" C-m
 tmux send-keys "roslaunch orb_slam2_ros orb_slam2_webcam.launch" C-m
 
 tmux selectp -t 0
